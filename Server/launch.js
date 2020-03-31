1bb9cd01508a8f662fdafa5e45d130bf5211fb93
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2020-03-31 14:11:42 +0200

    Cleanup in code

--- Server/launch.js
+++ Server/launch.js
@@ -5,20 +5,12 @@ const fs = require("fs");
 const server = new Server(8000);
 
 function serve(req, res, resource){
-    res.writeHead(200);
-    res.write(fs.readFileSync(resource.fileLocation));
-}
-
-server.addResource(new ServerResource("GET", "../Website/index.html", "/", (req, res, resource) => {
     /* Telling the server that there exist a resource such as (a get request, with FileLocation "./index.html"), with URL "/") Below is what the server should respond,
      * when recieving the GET request.*/ 
     /* Indicates that the Server are expexted to recieve a GET request from client, and respond with respondcode "200" and deliver the requested file */
     res.writeHead(200);
     res.write(fs.readFileSync(resource.fileLocation));
-
-}));
-// const database = new Datastore('database.db');
-// database.loadDatabase();, hvis du genstarter serveren så ja, men det kan du fixe.
+}
 
 server.addResource(new ServerResource("POST", "./submit/database.data", "/submit/", (req, res, resource) => {
 
@@ -28,6 +20,9 @@ server.addResource(new ServerResource("POST", "./submit/database.data", "/submit
     //res.write(fs.readFileSync(resource.fileLocation));
 
 }));
+
+
+server.addResource(new ServerResource("GET", "../Website/index.html", "/", serve));
 server.addResource(new ServerResource("GET", "../Website/Scripts/canvas.js", "/Scripts/canvas.js", serve));
 server.addResource(new ServerResource("GET", "../Website/Style/index.css", "/Style/index.css", serve));
 
--- Server/server.js
+++ Server/server.js
@@ -16,24 +16,28 @@ exports.Server = class {
     http.createServer((req, res) => {
 
       console.log(`REQUEST RECIEVED:\n\tmethod: ${req.method}\n\turl: ${req.url}`);
-      
+      let resourceFound = false;
       /* The "for-loop" iterates over the resource array, and chooses the one, which is requested. */
-      
+
       for (let resource of this.resources) {
         if (req.method === resource.method && req.url === resource.url) {
+          resourceFound = true;
           console.log(`FOUND RESOURCE MATCH: ${resource}`);
+          
             
           /*The resources callback is then used to figure out the appropriate response*/
           resource.callback(req, res, resource);
 
           res.end();
-          return;
         }
       }     
-          
-      res.writeHead(404);
-      //res.write(fs.readFileSync(404_PAGE_FILE_LOCATION));  TODO: 404 webpage in case 
-      res.end();
+      
+      if(!resourceFound) {
+        res.writeHead(404);
+        //res.write(fs.readFileSync(404_PAGE_FILE_LOCATION));  TODO: 404 webpage in case 
+        res.end();
+      }
+      
 
     }).listen(this.port);
     console.log("Session started!");
