c820a8923e09bf23c06c189c0653cca4c0a7dbbf
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2020-03-31 16:21:11 +0200

    addEntry in Datahandler

--- Server/DataHandler.js
+++ Server/DataHandler.js
@@ -1,17 +1,29 @@
 const fs = require("fs");
 
 exports.DataHandler = class {
-    constructor (fileLocation, coloumns=400) {
+    constructor (fileLocation, coloumns=1200) {
 
         FileLocCheck(fileLocation);
 
         this.fileLocation = fileLocation;
         this.coloumns = coloumns;
-        this.rows = 0;
+        this.rows = 0; /*TODO: function LoadRows() should be created and should read how many current entry rows is in {fileLocation} */
+    }
+
+    addEntry(entry) {
+      const writeStream = fs.createWriteStream(this.fileLocation, {start: 0, flags: "r+"});
+      this.rows++;
+      writeStream.write(this.rows, (err) => {
+        if(err){
+          console.log(err);
+        }
+        writeStream.end();
+      });
+      
+      /*TODO: Appending of entry into file*/
     }
 
 }
-
 
 /* Checks if the file exists */
 function FileLocCheck(fileLocation) {
