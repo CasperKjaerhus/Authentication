1c0c967fe4c5c4d9dbd4bd2b70203c5a9b0e7f41
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2020-04-23 13:47:08 +0200

    DataHandler PrepareNNData done

--- Server/DataHandler.js
+++ Server/DataHandler.js
@@ -2,56 +2,92 @@ const fs = require("fs");
 const readline = require("readline");
 
 exports.DataHandler = class {
-    constructor (fileLocation, coloumns=1200) {
-        this.coloumns = coloumns;
-        this.fileLocation = fileLocation;
-        this.rows = null;
-        PathCheck(fileLocation, coloumns);
-    }
-
-    /* Function to initialise class, this has to be async since we need to wait for LoadRows to finish.*/
-    async init() {
-        await CountRows(this.fileLocation).then((result) => {
-          this.rows = result;
-        });   
-    }
+  constructor () {}
 
     /*Adds a data entry into fileLocation*/
-    async addEntry(entry) {
-      let data;
+  static async addEntry(entry, username) {
+    let data = `${entry}\n`;
+    let fileLocation = `./data/${username}/drawings.txt`;
 
-      if (this.rows > 0) {
-        data = `\n${entry}`;
-      } else {
-        data = entry;
+    UserPathCheck(`./data/${username}`);
+
+    fs.appendFile(fileLocation, data, (err) => {
+      console.log(`Error: ${err}`);
+    });
+  }
+
+  static JSONToData(dataJSONString) {
+      
+    const dataObject = JSON.parse(dataJSONString);
+    let dataString = "";
+
+    for (let i = 0; i < dataObject.xArray.length; i++) {
+      dataString += `${dataObject.xArray[i]} ${dataObject.yArray[i]} ${dataObject.timeStamps[i]} ${dataObject.gradArray[i]}`; /* What actually goes into gradArray? What is the input?*/
+
+      if (i <= dataObject.xArray.length-1) {
+        dataString += " "; /*Adds a whitespace between*/
       }
-
-      fs.appendFile(this.fileLocation, data, (err) =>{
-        console.log(`Error: ${err}`);
-      })
-      this.rows++;
     }
 
-    static JSONToData(dataJSONString) {
+    return dataString;
+  }
 
-      const dataObject = JSON.parse(dataJSONString);
-      let dataString = "";
+  static async prepareNNData(username, coloumns){
+    UserPathCheck(`./data/${username}`);
+    let rows;
+    
+    /*receives the amount of rows/lines and uses this alongside the amount of coloums to write the start of the NNData file*/
+    await CountRows(`./data/${username}/drawings.txt`).then((val) => rows = val);
+    console.log("got here");
 
-      for (let i = 0; i < dataObject.xArray.length; i++) {
-        dataString += `${dataObject.xArray[i]} ${dataObject.yArray[i]} ${dataObject.timeStamps[i]} ${dataObject.gradArray[i]}`; /* What actually goes into gradArray? What is the input?*/
-        
-        if (i <= dataObject.xArray.length-1) {
-          dataString += " "; /*Adds a whitespace between*/
-        }
-      }
+    fs.appendFile(`./data/${username}/NNData.txt`, `${rows} ${coloumns}\n`, (err) => console.log(err));
+    
+    
+    const prom = new Promise((resolve, reject) => {
 
-      return dataString;
-    }
+      const readlineInterface = readline.createInterface({
+        input: fs.createReadStream(`./data/${username}/drawings.txt`)
+      });
+      /*Line event fires when a line is read from the file, and is used to copy to another file*/
+      readlineInterface.on("line", (input) => {
+        fs.appendFile(`./data/${username}/NNData.txt`, input+"\n", (err) => console.log(err))
+      });
+      
+      /*Close event fires upon end of file and is used to delete the last new line and resolves the promise*/
+      readlineInterface.on("close", () => {
+        fs.stat(`./data/${username}/NNData.txt`, (err, stats) => {
+          fs.truncate(`./data/${username}/NNData.txt`, stats.size - 1, (err) => {});
+        })
+        resolve();
+      });
+    })
+    await prom.then(() => console.log(`Done writing from [./data/${username}/drawings.txt] to [./data/${username}/NNData.txt]`));
+  }
+}
+
+/*checks if a folder with the users username exists*/
+function UserPathCheck(folderLocation){
+  if(fs.existsSync(folderLocation) === false) {
+    console.log("User folder does not exist")
+    fs.mkdirSync(folderLocation);
+    console.log("User folder created");
+
+    Userfilecheck(folderLocation, "drawings.txt");
+    Userfilecheck(folderLocation, "NNData.txt");
+  }
+  
+}
+function Userfilecheck(folderLocation, filename){
+  if(fs.existsSync(`${folderLocation}/${filename}`) === false) {
+    console.log(`${filename} does not exist`)
+    fs.writeFileSync(`${folderLocation}/${filename}`, "");
+    console.log(`${filename} created`);
+  }
 }
 
 
 
-
+/*The amount of rows/lines in the document is counted and then the number is returned*/
 async function CountRows(fileLocation) {
   let returnData = 0; 
   const prom = new Promise((resolve, reject) => {
@@ -62,17 +98,17 @@ async function CountRows(fileLocation) {
     });
 
     readlineInterface.on("line", (input) => {
+      console.log(linenum);
       linenum++;
     });
     
-    readlineInterface.on("end", () => {
-      readlineInterface.close();
+    readlineInterface.on("close", () => {
+      console.log("test");
       resolve(linenum);
     }); 
   });
 
   await prom.then((val) => {
-    console.log(`SlicedInput Val: ${val}`);
     returnData = val;
   }).catch((err) => {
     console.error(err);
