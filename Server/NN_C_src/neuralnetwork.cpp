44af23c8bac03c45d812b11b48a55436d26592e9
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2020-05-02 00:31:13 +0200

    Cleaning up + moving around
    General formatting improvements!

--- Server/NN_C_src/neuralnetwork.cpp
+++ Server/NN_C_src/neuralnetwork.cpp
@@ -9,11 +9,25 @@
 
 namespace neuralnetwork {
 
+/*Function headers*/
 napi_status makeMatrixNodeObj(napi_env env, matrix* mat, napi_value* matrixObj);
 napi_value loadMatrix(napi_env env, napi_callback_info cbinfo);
 
 
+napi_value init(napi_env env, napi_value exports) {
+  napi_status status;
+  napi_value loadMatrixFn;
 
+  status = napi_create_function(env, NULL, 0, loadMatrix, NULL, &loadMatrixFn);
+  if(status != napi_ok) return NULL;
+
+  status = napi_set_named_property(env, exports, "loadMatrix", loadMatrixFn);
+  if (status != napi_ok) return NULL;
+
+  return exports;
+}
+
+/*exported functions below here*/
 napi_value loadMatrix(napi_env env, napi_callback_info cbinfo) {
 
   char* fileLocation;
@@ -47,9 +61,18 @@ napi_value loadMatrix(napi_env env, napi_callback_info cbinfo) {
   return matrixObj;
 }
 
+
+/*Non-exported functions below here*/
+
+/****************************************************************************/
+/*Makes a JavaScript object from a matrix                                   */
+/* {IN} env         - N-API environment context                             */
+/* {IN} mat         - Matrix struct                                         */
+/* {OUT} matrixObj  - N-API value pointer pointing to the JavaScript object */
+/****************************************************************************/
 napi_status makeMatrixNodeObj(napi_env env, matrix* mat, napi_value* matrixObj){
   napi_status status;
-  napi_value rows, cols, valArray;
+  napi_value rows, cols, valArray, rowoffsetArray;
   /*Creating a JavaScript Object that should hold our matrix*/
   status = napi_create_object(env, matrixObj);
   if(status != napi_ok) return status;
@@ -84,20 +107,24 @@ napi_status makeMatrixNodeObj(napi_env env, matrix* mat, napi_value* matrixObj){
   status = napi_set_named_property(env, *matrixObj, "values", valArray);
   if(status != napi_ok) return status;
 
+  status = napi_create_array_with_length(env, mat->cols, &rowoffsetArray);
+  if(status != napi_ok) return status;
+
+  /*Fills the array with matrix values*/
+  for(int i = 0; i < mat->cols; i++){
+
+    napi_value element;
+    status = napi_create_int32(env, mat->rowoffset[i], &element);
+    if(status != napi_ok) return status;
+
+    status = napi_set_element(env, rowoffsetArray, i, element);
+    if(status != napi_ok) return status;
+  }
+
+  status = napi_set_named_property(env, *matrixObj, "rowoffset", rowoffsetArray);
+  if(status != napi_ok) return status;
+
   return napi_ok;
-}
-
-napi_value init(napi_env env, napi_value exports) {
-  napi_status status;
-  napi_value loadMatrixFn;
-
-  status = napi_create_function(env, NULL, 0, loadMatrix, NULL, &loadMatrixFn);
-  if(status != napi_ok) return NULL;
-
-  status = napi_set_named_property(env, exports, "loadMatrix", loadMatrixFn);
-  if (status != napi_ok) return NULL;
-
-  return exports;
 }
 
 /*Module added to node through init function*/
